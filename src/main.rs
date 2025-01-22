993b98d7da81aa3f443d9373a27d44345247adc4
Adam Eury <euryadam@gmail.com>
2025-01-21 22:12:31 -0500

    Remove global static mutable variable.

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,57 @@
 use anyhow::Result;
 use nix::sys::termios::{
-    self, ControlFlags, InputFlags, LocalFlags, OutputFlags, SpecialCharacterIndices,
+    self, ControlFlags, InputFlags, LocalFlags, OutputFlags, SpecialCharacterIndices, Termios,
 };
 use std::{io::Read, os::fd::AsFd};
 
-static mut ORIG_TERMIOS: Option<termios::Termios> = None;
-
-fn enable_raw_mode(stdin: &std::io::Stdin) -> Result<()> {
-    let orig = termios::tcgetattr(stdin.as_fd())?;
-    let mut raw = orig.clone();
-    unsafe {
-        ORIG_TERMIOS = Some(orig.clone());
-    }
-
-    // Disable input flags
-    // BRKINT, ICRNL, INPCK, ISTRIP, IXON
-    termios::InputFlags::remove(&mut raw.input_flags, InputFlags::BRKINT);
-    termios::InputFlags::remove(&mut raw.input_flags, InputFlags::ICRNL);
-    termios::InputFlags::remove(&mut raw.input_flags, InputFlags::INPCK);
-    termios::InputFlags::remove(&mut raw.input_flags, InputFlags::ISTRIP);
-    termios::InputFlags::remove(&mut raw.input_flags, InputFlags::IXON);
-
-    // Disable output flags
-    termios::OutputFlags::remove(&mut raw.output_flags, OutputFlags::OPOST);
-
-    // Set the control flags to 8 data bits, no parity, no stop bits.
-    termios::ControlFlags::set(&mut raw.control_flags, ControlFlags::CS8, true);
-
-    // Disable local flags
-    termios::LocalFlags::remove(&mut raw.local_flags, LocalFlags::ECHO);
-    termios::LocalFlags::remove(&mut raw.local_flags, LocalFlags::ICANON);
-    termios::LocalFlags::remove(&mut raw.local_flags, LocalFlags::IEXTEN);
-    termios::LocalFlags::remove(&mut raw.local_flags, LocalFlags::ISIG);
-
-    // Set the minimum number of characters to read before returning from read()
-    // and the timeout in deciseconds.
-    raw.control_chars[SpecialCharacterIndices::VMIN as usize] = 0;
-    raw.control_chars[SpecialCharacterIndices::VTIME as usize] = 1;
-
-    // Apply the new settings
-    termios::tcsetattr(stdin.as_fd(), termios::SetArg::TCSAFLUSH, &raw)?;
-
-    Ok(())
+struct Terminal {
+    orig: Termios,
+    curr: Termios,
 }
 
-fn disable_raw_mode(stdin: &std::io::Stdin) -> Result<()> {
-    let orig = unsafe { ORIG_TERMIOS.clone() };
-    if let Some(orig) = orig {
-        termios::tcsetattr(stdin.as_fd(), termios::SetArg::TCSAFLUSH, &orig)?;
+impl Terminal {
+    fn new() -> Self {
+        let orig = termios::tcgetattr(std::io::stdin().as_fd()).unwrap();
+        let curr = orig.clone();
+        Self { orig, curr }
     }
-    Ok(())
+
+    fn enable_raw_mode(&mut self, stdin: &std::io::Stdin) -> Result<()> {
+        // Disable input flags
+        // BRKINT, ICRNL, INPCK, ISTRIP, IXON
+        termios::InputFlags::remove(&mut self.curr.input_flags, InputFlags::BRKINT);
+        termios::InputFlags::remove(&mut self.curr.input_flags, InputFlags::ICRNL);
+        termios::InputFlags::remove(&mut self.curr.input_flags, InputFlags::INPCK);
+        termios::InputFlags::remove(&mut self.curr.input_flags, InputFlags::ISTRIP);
+        termios::InputFlags::remove(&mut self.curr.input_flags, InputFlags::IXON);
+
+        // Disable output flags
+        termios::OutputFlags::remove(&mut self.curr.output_flags, OutputFlags::OPOST);
+
+        // Set the control flags to 8 data bits, no parity, no stop bits.
+        termios::ControlFlags::set(&mut self.curr.control_flags, ControlFlags::CS8, true);
+
+        // Disable local flags
+        termios::LocalFlags::remove(&mut self.curr.local_flags, LocalFlags::ECHO);
+        termios::LocalFlags::remove(&mut self.curr.local_flags, LocalFlags::ICANON);
+        termios::LocalFlags::remove(&mut self.curr.local_flags, LocalFlags::IEXTEN);
+        termios::LocalFlags::remove(&mut self.curr.local_flags, LocalFlags::ISIG);
+
+        // Set the minimum number of characters to read before returning from read()
+        // and the timeout in deciseconds.
+        self.curr.control_chars[SpecialCharacterIndices::VMIN as usize] = 0;
+        self.curr.control_chars[SpecialCharacterIndices::VTIME as usize] = 1;
+
+        // Apply the new settings
+        termios::tcsetattr(stdin.as_fd(), termios::SetArg::TCSAFLUSH, &self.curr)?;
+
+        Ok(())
+    }
+
+    fn disable_raw_mode(&mut self, stdin: &std::io::Stdin) -> Result<()> {
+        termios::tcsetattr(stdin.as_fd(), termios::SetArg::TCSAFLUSH, &self.orig)?;
+        Ok(())
+    }
 }
 
 fn ctrl_key(c: char) -> u8 {
@@ -63,8 +65,10 @@ fn editor_read_key(handle: &mut std::io::StdinLock<'_>) -> Result<u8> {
 }
 
 fn main() -> Result<()> {
+    let mut terminal = Terminal::new();
+
     let stdin = std::io::stdin();
-    enable_raw_mode(&stdin)?;
+    terminal.enable_raw_mode(&stdin)?;
 
     let mut handle = stdin.lock();
 
@@ -82,7 +86,7 @@ fn main() -> Result<()> {
         }
     }
 
-    disable_raw_mode(&stdin)?;
+    terminal.disable_raw_mode(&stdin)?;
 
     Ok(())
 }
